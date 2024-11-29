3781ae7597aee2e5f8f1a18c54dbd82ad298cffa
Kethaka Wanasinghe <152339243+KethakaWanasinghe@users.noreply.github.com>
2024-11-30 03:22:59 +0530

    Complete the header part
    More small changes are upcoming

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -9,6 +9,14 @@ const Header = () => {
             <Image src={assets.logo} width={180} alt='Logo' className='w-[130px] sm:w-auto' />
             <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000] ' >Get Started <Image src={assets.arrow} alt='Arrow'/> </button>
         </div>
+        <div className='text-center my-8'>
+          <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blog</h1>
+          <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]'>
+            <input type="email" placeholder='Enter Your Email' className='pl-4 outline-none'/>
+            <button type='submit' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white'>Subscribe</button>
+          </form>
+        </div>
     </div>
   )
 }
