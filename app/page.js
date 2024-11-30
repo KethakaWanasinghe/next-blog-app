9a521bbd26cf31330ba2ed58cd3412658c53d35e
Kethaka Wanasinghe <152339243+KethakaWanasinghe@users.noreply.github.com>
2024-12-01 00:57:51 +0530

    Bloglist
    Selection of category is updated

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 'use client'
-import Blogitem from "@/Components/Blogitem";
+import Bloglist from "@/Components/Bloglist";
 import Header from "@/Components/Header";
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <Blogitem/>
+      <Bloglist/>
     </>
   );
 }
