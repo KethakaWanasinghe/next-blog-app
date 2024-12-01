877cd75a94464678f65aaff1d2f5303c5b248381
Kethaka Wanasinghe <152339243+KethakaWanasinghe@users.noreply.github.com>
2024-12-02 00:31:43 +0530

    Footer
    Home page complete

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 'use client'
 import Bloglist from "@/Components/Bloglist";
+import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Header/>
       <Bloglist/>
+      <Footer/>
     </>
   );
 }
